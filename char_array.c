44fca1796df6d4c64b7caa3bcd7661584d216ab5
DSKim <108538247+CLKim00@users.noreply.github.com>
2023-05-17 22:54:20 +0900

    Array

---
+++ char_array.c
@@ -0,0 +1,15 @@
+// 문자열을 저장하는 char형 배열
+
+#include <stdio.h>
+
+int main(void)
+{
+    char str[80] = "applejam";          // 문자열 초기화
+    
+    printf("최초 문자열 : %s\n", str);     // 초기화 문자열 출력
+    printf("문자열 입력 : ");
+    scanf("%s", str);
+    printf("입력 후 문자열 : %s\n", str);   // 입력된 문자열 출력
+
+    return 0;
+}
