19be25ec8b61da8e444b92424f2019676cfd645d
DSKim <108538247+CLKim00@users.noreply.github.com>
2023-05-17 22:32:02 +0900

    Operator

---
+++ 관계연산자.c
@@ -0,0 +1,27 @@
+// 관계 연산의 결괏값 확인
+
+#include <stdio.h>
+
+int main(void)
+{
+    int a = 10, b = 20, c = 10;
+    int res;                    // 결괏값을 저장할 변수
+                                // 각각 a와 b, c 값을 대입해보자.
+    
+    res = (a > b);              // 10 > 20은 거짓이므로 결괏값은 0
+    printf("a > b : %d\n", res);
+    res = (a >= b);             // 10 >= 20은 거짓
+    printf("a >= b : %d\n", res);
+    res = (a < b);              // 10 < 20은 참이므로 결괏값은 1
+    printf("a < b : %d\n", res);
+    res = (a <= b);             // 10 <= 20은 참
+    printf("a <= b : %d\n", res);
+    res = (a <= 20);            // 10 <= 10은 참
+    printf("a <= c : %d\n", res);
+    res = (a == b);             // 10 == 20은 거짓
+    printf("a == b : %d\n", res);
+    res = (a != c);             // 10 != 10은 거짓
+    printf("a != c : %d\n", res);
+
+    return 0;
+}
